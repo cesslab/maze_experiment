6b4b611c8090347c1f3d868f76152c0ba697d3b2
cesslab <anwarruff@gmail.com>
2018-10-25 14:55:40 -0400

    Added template vars

--- time_allocation/pages.py
+++ time_allocation/pages.py
@@ -4,7 +4,15 @@ from .models import Constants
 
 
 class TimeAllocationPage(Page):
-    pass
+    form_model = 'player'
+    form_fields = ['left_lottery_id', 'left_lottery_time', 'right_lottery_time', 'right_lottery_time']
+
+    def vars_for_template(self):
+        lottery_pair = self.participant.vars['lotteries'][self.round_number - 1]
+        return {
+            'l': lottery_pair[0],
+            'r': lottery_pair[1],
+        }
 
 
 class ResultsWaitPage(WaitPage):
