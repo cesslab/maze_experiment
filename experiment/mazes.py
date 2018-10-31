a273586d92081a20addf81da28d357143d1f9ae6
cesslab <anwarruff@gmail.com>
2018-10-31 14:37:23 -0400

    Added maze class

---
+++ experiment/mazes.py
@@ -0,0 +1,7 @@
+class Maze:
+    def __init__(self, name: str, start_x: int, start_y: int, end_x: int, end_y: int):
+        self.name = name
+        self.start_x = start_x
+        self.start_y = start_y
+        self.end_x = end_x
+        self.end_y = end_y
