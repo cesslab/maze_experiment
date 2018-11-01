c2cd933c534dc7fb76943e1c193aedabd9dc67e7
cesslab <anwarruff@gmail.com>
2018-11-01 14:54:06 -0400

    Added phase 5: maze play with chosen time

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ SESSION_CONFIGS = [
        'name': 'maze_experiment',
        'display_name': "Maze Experiment",
        'num_demo_participants': 1,
-       'app_sequence': ['preference', 'time_allocation', 'tasks', 'play_maze_fixed_time'],
+       'app_sequence': ['preference', 'time_allocation', 'tasks', 'play_maze_fixed_time', 'play_maze_chosen_time'],
        'max_time_seconds': 120,
        'max_maze_solve_time': 240,
     },
