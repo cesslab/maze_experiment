3509d9ed957b66156480b5524cd9bd3527f9fd79
aaruff <anwarruff@gmail.com>
2019-10-07 13:47:41 -0400

    added instructions and maze paractice pages

--- practice_maze/pages.py
+++ practice_maze/pages.py
@@ -2,18 +2,30 @@ from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
 
+from experiment.mazes import Maze
 
-class MyPage(Page):
+
+class Instructions(Page):
     pass
 
 
-class ResultsWaitPage(WaitPage):
-    def after_all_players_arrive(self):
-        pass
+class PracticeMaze(Page):
+    form_model = 'player'
+    form_fields = ['solved', 'solve_time_seconds', 'maze_id']
+
+    def vars_for_template(self):
+        maze = Maze('closed', 147, 2, 169, 314)
 
 
-class Results(Page):
-    pass
+        return {
+            'seconds_to_solve': 120,
+            'maze_img': 'practice_maze/img/'+maze.name+'.png',
+            'maze_id': maze.name,
+            'start_x': maze.start_x,
+            'start_y': maze.start_y,
+            'end_x': maze.end_x,
+            'end_y': maze.end_y,
+        }
 
 
-page_sequence = [MyPage, ResultsWaitPage, Results]
+page_sequence = [Instructions, PracticeMaze]
