1640a9a9d97bec18a927e62959efc027a724f2b4
aaruff <anwarruff@gmail.com>
2019-10-09 15:47:48 -0400

    added pair ids for part 1 and 2, and updated lottery maze ids

--- preference/models.py
+++ preference/models.py
@@ -20,20 +20,20 @@ class Subsession(BaseSubsession):
             for player in self.get_players():
                 lotteries = [
                     [
-                        Lottery(1, c(8), c(4), [50], 50, 50, Maze('easy1', 147, 2, 169, 314)),
-                        Lottery(2, c(8), c(4), [50], 60, 40, Maze('easy2', 147, 2, 169, 314))
+                        Lottery(1, c(8), c(4), [50], 50, 50, Maze('40_40_1', 147, 2, 169, 314)),
+                        Lottery(2, c(8), c(4), [50], 60, 40, Maze('60_40_1', 147, 2, 169, 314))
                     ],
                     [
-                        Lottery(3, c(8), c(4), [50], 80, 20, Maze('easy3', 147, 2, 169, 314)),
-                        Lottery(4, c(8), c(4), [50], 80, 20, Maze('easyM1', 147, 2, 169, 314))
+                        Lottery(3, c(8), c(4), [50], 80, 20, Maze('40_40_1', 147, 2, 169, 314)),
+                        Lottery(4, c(8), c(4), [50], 80, 20, Maze('60_40_1', 147, 2, 169, 314))
                     ],
                     [
-                        Lottery(5, c(8), c(4), [40, 60], 80, 20, Maze('easyM2', 147, 2, 169, 314)),
-                        Lottery(6, c(10), c(2), [50], 60, 40, Maze('easyM3', 147, 2, 169, 314))
+                        Lottery(5, c(8), c(4), [40, 60], 80, 20, Maze('40_40_1', 147, 2, 169, 314)),
+                        Lottery(6, c(10), c(2), [50], 60, 40, Maze('60_40_1', 147, 2, 169, 314))
                     ],
                     [
-                        Lottery(7, c(10.44), c(2), [30], 60, 40, Maze('hard1', 147, 2, 169, 314)),
-                        Lottery(8, c(8), c(4), [80], 60, 40, Maze('hard2', 147, 2, 169, 314))
+                        Lottery(7, c(10.44), c(2), [30], 60, 40, Maze('40_40_1', 147, 2, 169, 314)),
+                        Lottery(8, c(8), c(4), [80], 60, 40, Maze('60_40_1', 147, 2, 169, 314))
                     ],
                 ]
                 random.shuffle(lotteries)
@@ -41,8 +41,8 @@ class Subsession(BaseSubsession):
                     random.shuffle(pair)
                 player.participant.vars['preferred_lotteries'] = lotteries
                 # randomly choose one of the pairs for payment
-                player.participant.vars['preferred_pair_id'] = random.randint(1, Constants.num_rounds)
-                player.participant.vars['timed_pair_id'] = random.randint(1, Constants.num_rounds)
+                player.participant.vars['preferred_pair_id'] = random.randint(0, Constants.num_rounds-1)
+                player.participant.vars['timed_pair_id'] = random.randint(0, Constants.num_rounds-1)
 
 
 class Group(BaseGroup):
