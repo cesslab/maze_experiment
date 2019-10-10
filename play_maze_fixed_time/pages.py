88895ddcf9f38d832a04c5efc668aee86b0a319d
aaruff <anwarruff@gmail.com>
2019-10-10 15:59:35 -0400

    added lottery pair to the template dict

--- play_maze_fixed_time/pages.py
+++ play_maze_fixed_time/pages.py
@@ -2,7 +2,8 @@ from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
 
-from experiment.lottery import Lottery
+from experiment.lottery import Lottery, LotteryPreferencePair, LotteryPreferencePairCollection
+from experiment.mazes import Maze
 
 
 class Instructions(Page):
@@ -10,13 +11,15 @@ class Instructions(Page):
         return self.round_number == 1
 
     def vars_for_template(self):
-        maze_ids = []
-        for lottery_pair in self.participant.vars['preferred_lottery_collection']:
-            for lottery in lottery_pair:
-                maze_ids.append(lottery.maze.name)
+        lottery_collection: LotteryPreferencePairCollection = self.participant.vars['preferred_lottery_collection']
+        lottery_pair: LotteryPreferencePair = lottery_collection.round_pair(self.round_number)
 
         return {
-            'maze_ids': maze_ids,
+            'maze_ids': lottery_pair.maze_names(),
+            'left_lottery': lottery_pair.left_lottery,
+            'right_lottery': lottery_pair.right_lottery,
+            'realized_lottery': lottery_pair.realized_lottery,
+            'preference': lottery_pair.preference,
         }
 
 
@@ -25,15 +28,9 @@ class MazePage(Page):
     form_fields = ['solved', 'solve_time_seconds', 'maze_id']
 
     def vars_for_template(self):
-        lottery_pair = self.participant.vars['preferred_lottery_collection'][self.round_number - 1]
-        left_lottery: Lottery = lottery_pair[0]
-        right_lottery: Lottery = lottery_pair[1]
-
-        if left_lottery.is_preference:
-            maze = left_lottery.maze
-        else:
-            maze = right_lottery.maze
-
+        lottery_collection: LotteryPreferencePairCollection = self.participant.vars['preferred_lottery_collection']
+        lottery_pair: LotteryPreferencePair = lottery_collection.round_pair(self.round_number)
+        maze: Maze = lottery_pair.realized_lottery.maze
         return {
             'seconds_to_solve': self.session.config['max_maze_solve_time'],
             'maze_img': 'play_maze_fixed_time/img/'+maze.name+'.png',
