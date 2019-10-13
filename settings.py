6d1b7d82847178f2c34c513a035851e3286687df
aaruff <anwarruff@gmail.com>
2019-10-13 18:42:44 -0400

    updated timing

--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ SESSION_CONFIGS = [
            'preference', 'time_allocation', 'tasks', 'practice_maze', 'play_maze_fixed_time', 'play_maze_chosen_time', 'payoffs'
        ],
        'max_time_seconds': 120,
-       'max_maze_solve_time': 120,
+       'max_maze_solve_time': 60,
     },
 ]
 
