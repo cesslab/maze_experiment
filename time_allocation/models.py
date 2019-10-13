5ca6d32aeced6dbe801e93ebb808af8c2527881e
aaruff <anwarruff@gmail.com>
2019-10-13 17:50:55 -0400

    moved the timed lottery pair collection to the TimeLotteryPairCollection class __init__ func

--- time_allocation/models.py
+++ time_allocation/models.py
@@ -18,25 +18,7 @@ class Constants(BaseConstants):
 class Subsession(BaseSubsession):
     def creating_session(self):
         for player in self.get_players():
-            lottery_pairs = TimedLotteryPairCollection([
-                LotteryTimedPair(
-                    Lottery(1, c(8), c(4), [50], 50, 50, Maze('40_40_1', 147, 2, 169, 314)),
-                    Lottery(2, c(8), c(4), [50], 60, 40, Maze('60_40_1', 147, 2, 169, 314))
-                ),
-                LotteryTimedPair(
-                    Lottery(3, c(8), c(4), [50], 80, 20, Maze('40_40_1', 147, 2, 169, 314)),
-                    Lottery(4, c(8), c(4), [50], 80, 20, Maze('60_40_1', 147, 2, 169, 314))
-                ),
-                LotteryTimedPair(
-                    Lottery(5, c(8), c(4), [40, 60], 80, 20, Maze('40_40_1', 147, 2, 169, 314)),
-                    Lottery(6, c(10), c(2), [50], 60, 40, Maze('60_40_1', 147, 2, 169, 314))
-                ),
-                LotteryTimedPair(
-                    Lottery(7, c(10.44), c(2), [30], 60, 40, Maze('40_40_1', 147, 2, 169, 314)),
-                    Lottery(8, c(8), c(4), [80], 60, 40, Maze('60_40_1', 147, 2, 169, 314))
-                ),
-            ])
-            player.participant.vars['timed_lottery_collection'] = lottery_pairs
+            player.participant.vars['time_lottery_pair_collection'] = TimedLotteryPairCollection()
 
 
 class Group(BaseGroup):
