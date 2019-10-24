4e5dd9d330cc5100c3e528a9dc03548f0796447e
aaruff <anwarruff@gmail.com>
2019-10-24 13:55:58 -0400

    removed unused imports

--- time_allocation/pages.py
+++ time_allocation/pages.py
@@ -1,7 +1,5 @@
-from otree.api import Currency as c, currency_range
-from ._builtin import Page, WaitPage
-from .models import Constants
-from experiment.lottery import Lottery, LotteryTimedPair, TimedLotteryPairCollection
+from ._builtin import Page
+from experiment.lottery import LotteryTimedPair, TimedLotteryPairCollection
 
 
 class Instructions(Page):
