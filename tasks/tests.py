c046a756d6e84423ad262ff4d073633235db9093
aaruff <anwarruff@gmail.com>
2019-10-24 15:22:25 -0400

    Added tests for task 1 and task 2

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,10 +1,32 @@
-from otree.api import Currency as c, currency_range
-from . import pages
+import random
+
 from ._builtin import Bot
-from .models import Constants
+from otree.api import expect, SubmissionMustFail
+
+from .pages import TaskOne, TaskTwoPage, TaskThree, TaskFour, TaskFive, TaskSix, TaskSeven, Instructions
+from experiment.tasks import TaskTwo
 
 
 class PlayerBot(Bot):
-
     def play_round(self):
-        pass
+        yield (Instructions)
+
+        # Test task 1
+        SubmissionMustFail(TaskOne, {'task_one_choice': random.randint(4, 100)})
+        yield(TaskOne, {'task_one_choice': random.choice([1, 2, 3])})
+
+        # Test task 2
+        incorrect_form_field_values = {
+            'task_two_1': '', 'task_two_2': '', 'task_two_3': '', 'task_two_4': '', 'task_two_5': '',
+            'task_two_6': '', 'task_two_7': '', 'task_two_8': '', 'task_two_9': '', 'task_two_10': ''
+        }
+        yield(SubmissionMustFail(TaskTwoPage, incorrect_form_field_values))
+
+        correct_form_field_values = {
+            'task_two_1': random.choice([1, 2]), 'task_two_2': random.choice([1, 2]),
+            'task_two_3': random.choice([1, 2]), 'task_two_4': random.choice([1, 2]),
+            'task_two_5': random.choice([1, 2]), 'task_two_6': random.choice([1, 2]),
+            'task_two_7': random.choice([1, 2]), 'task_two_8': random.choice([1, 2]),
+            'task_two_9': random.choice([1, 2]), 'task_two_10': random.choice([1, 2]),
+        }
+        yield(TaskTwoPage, correct_form_field_values)
