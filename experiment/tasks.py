8c81d64deb86f41b9db0e426d9ef425571091813
aaruff <anwarruff@gmail.com>
2020-02-12 15:21:51 -0500

    Updated task and option order

--- experiment/tasks.py
+++ experiment/tasks.py
@@ -114,16 +114,16 @@ class TaskBeta:
 class TaskEpsilon:
     def __init__(self):
         self.cases: List[Case] = [
-            Case(Option(Option.A, 10, c(2.0), 90, c(1.6)), Option(Option.B, 10, c(3.85), 90, c(.1))),
-            Case(Option(Option.A, 20, c(2.0), 80, c(1.6)), Option(Option.B, 20, c(3.85), 80, c(.1))),
-            Case(Option(Option.A, 30, c(2.0), 70, c(1.6)), Option(Option.B, 30, c(3.85), 70, c(.1))),
-            Case(Option(Option.A, 40, c(2.0), 60, c(1.6)), Option(Option.B, 40, c(3.85), 60, c(.1))),
-            Case(Option(Option.A, 50, c(2.0), 50, c(1.6)), Option(Option.B, 50, c(3.85), 50, c(.1))),
-            Case(Option(Option.A, 60, c(2.0), 40, c(1.6)), Option(Option.B, 60, c(3.85), 40, c(.1))),
-            Case(Option(Option.A, 70, c(2.0), 30, c(1.6)), Option(Option.B, 70, c(3.85), 30, c(.1))),
-            Case(Option(Option.A, 80, c(2.0), 20, c(1.6)), Option(Option.B, 80, c(3.85), 20, c(.1))),
+            Case(Option(Option.A, 100, c(2.0), 0, c(1.6)), Option(Option.B, 100, c(3.85), 0, c(.1))),
             Case(Option(Option.A, 90, c(2.0), 10, c(1.6)), Option(Option.B, 90, c(3.85), 10, c(.1))),
-            Case(Option(Option.A, 100, c(2.0), 0, c(1.6)), Option(Option.B, 10, c(3.85), 0, c(.1))),
+            Case(Option(Option.A, 80, c(2.0), 20, c(1.6)), Option(Option.B, 80, c(3.85), 20, c(.1))),
+            Case(Option(Option.A, 70, c(2.0), 30, c(1.6)), Option(Option.B, 70, c(3.85), 30, c(.1))),
+            Case(Option(Option.A, 60, c(2.0), 40, c(1.6)), Option(Option.B, 60, c(3.85), 40, c(.1))),
+            Case(Option(Option.A, 50, c(2.0), 50, c(1.6)), Option(Option.B, 50, c(3.85), 50, c(.1))),
+            Case(Option(Option.A, 40, c(2.0), 60, c(1.6)), Option(Option.B, 40, c(3.85), 60, c(.1))),
+            Case(Option(Option.A, 30, c(2.0), 70, c(1.6)), Option(Option.B, 30, c(3.85), 70, c(.1))),
+            Case(Option(Option.A, 20, c(2.0), 80, c(1.6)), Option(Option.B, 20, c(3.85), 80, c(.1))),
+            Case(Option(Option.A, 10, c(2.0), 90, c(1.6)), Option(Option.B, 10, c(3.85), 90, c(.1))),
         ]
         self.payoff_case_number = random.randint(1, len(self.cases))
         self._payoff_option = None
