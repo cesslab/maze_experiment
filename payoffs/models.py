451936fa5649042f2133810ee3cf2c14c2d6a02f
aaruff <anwarruff@gmail.com>
2020-02-18 13:24:52 -0500

    Added payoff relevant fields to the player model

--- payoffs/models.py
+++ payoffs/models.py
@@ -1,4 +1,4 @@
-from otree.api import BaseConstants, BaseSubsession, BaseGroup, BasePlayer
+from otree.api import BaseConstants, BaseSubsession, BaseGroup, BasePlayer, models
 
 author = 'Anwar A. Ruff'
 doc = """Payoffs"""
@@ -19,5 +19,17 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    pass
+    part_1_selected_lottery_pair_round_number = models.IntegerField()
+    part_1_selected_lottery_id = models.IntegerField()
+    part_1_selected_lottery_maze_solved = models.BooleanField()
+    part_1_random_value = models.IntegerField()
+    part_2_selected_lottery_pair_round_number = models.IntegerField()
+    part_2_left_maze_solved = models.BooleanField()
+    part_2_left_random_value = models.IntegerField()
+    part_2_right_maze_solved = models.BooleanField()
+    part_2_right_random_value = models.IntegerField()
+    part_3_payoff_task_number = models.IntegerField()
+    part_3_task_epsilon_payoff_case = models.IntegerField()
+    part_3_task_epsilon_option_selected = models.IntegerField()
+    part_3_task_epsilon_option_random_number = models.IntegerField()
 
--- payoffs/pages.py
+++ payoffs/pages.py
@@ -39,5 +39,34 @@ class Payoffs(Page):
             'participation_fee': self.session.config['participation_fee']
         }
 
+    def before_next_page(self):
+        timed_lottery_collection: TimedLotteryPairCollection = self.participant.vars['time_lottery_pair_collection']
+        timed_pair: LotteryTimedPair = timed_lottery_collection.selected_lottery_pair()
+
+        left_timed_lottery: Lottery = timed_pair.left_lottery
+        right_timed_lottery: Lottery = timed_pair.right_lottery
+
+        preferred_lottery_collection: PreferredLotteryPairCollection = self.participant.vars['preferred_lottery_pair_collection']
+        preferred_pair: LotteryPreferencePair = preferred_lottery_collection.selected_lottery_pair()
+
+        task_epsilon: TaskEpsilon = self.participant.vars['task_epsilon']
+        self.player.part_1_selected_lottery_pair_round_number = preferred_lottery_collection.selected_lottery_pair_round_number()
+        self.player.part_1_selected_lottery_id = preferred_pair.lottery_label
+        self.player.part_1_selected_lottery_maze_solved = preferred_pair.realized_lottery.maze.solved
+        self.player.part_1_random_value = preferred_pair.realized_lottery.random_value
+        self.player.part_2_selected_lottery_pair_round_number = timed_lottery_collection.selected_lottery_pair_round_number()
+        self.player.part_2_left_maze_solved = left_timed_lottery.maze.solved
+        self.player.part_2_left_random_value = left_timed_lottery.random_value
+        self.player.part_2_right_maze_solved = right_timed_lottery.maze.solved
+        self.player.part_2_right_random_value = right_timed_lottery.random_value
+        self.player.part_3_payoff_task_number = self.participant.vars['payment_task_number']
+        self.player.part_3_task_epsilon_payoff_case = task_epsilon.payoff_case_number
+        self.player.part_3_task_epsilon_option_selected = task_epsilon.payoff_option.option_type
+        self.player.part_3_task_epsilon_option_random_number = task_epsilon.payoff_option.random_num
+
+
+
+
+
 
 page_sequence = [Payoffs]
