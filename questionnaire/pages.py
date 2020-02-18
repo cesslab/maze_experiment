f81a2cf00e10002b164f39dc65bc7d0b111e1b63
aaruff <anwarruff@gmail.com>
2020-02-18 13:26:10 -0500

    added part 2 to the questionnaire

--- questionnaire/pages.py
+++ questionnaire/pages.py
@@ -2,10 +2,24 @@ from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
 
+from experiment.lottery import Lottery, Maze
+from otree.api import (
+    Currency
+)
+
 
 class PartOne(Page):
     form_model = 'player'
     form_fields = ['gender', 'age', 'major', 'year_in_college']
 
 
-page_sequence = [PartOne]
+class PartTwo(Page):
+    form_model = 'player'
+    form_fields = ['q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7']
+
+    def vars_for_template(self) -> dict:
+        return {
+            'l': Lottery(1, Currency(9), Currency(4), [50], 70, 40, Maze('40_40_1', 147, 2, 169, 314))
+        }
+
+page_sequence = [PartOne, PartTwo]
